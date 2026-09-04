8ba8dfccd6d2c08886bd96be70bab5aa18279af2
Saharad Chimdonthong <sahard.chd@gmail.com>
2026-09-04 14:47:18 +0700

    First meet Express
    Co-authored-by: SaharadMk <saharad.ch@gmail.com>

---
+++ backend/src/fakeDB/fakeUsers.js
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: "1",
+    username: "Neetibut",
+    email:"neeibut@exmaple.com",
+    password:"pass123"
+  },
+  {
+    id: "2",
+    username: "Mark",
+    email:"mark@exmaple.com",
+    password:"pass123"
+  },
+  {
+    id: "3",
+    username: "Bronya",
+    email:"bronya@exmaple.com",
+    password:"pass123"
+  },
+];
